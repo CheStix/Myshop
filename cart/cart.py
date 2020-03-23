f57e4e3b41d52a865dbde093af97df2ddc3e332e
CheStix <che.eldar@gmail.com>
2020-03-23 16:06:31 +0300

    added a coupon system for discounts

--- cart/cart.py
+++ cart/cart.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -13,6 +14,8 @@ class Cart(object):
             # save empty cart in session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # save coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self):
         """iterate over items in cart and get products from database"""
@@ -61,3 +64,17 @@ class Cart(object):
         # clear cart
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
